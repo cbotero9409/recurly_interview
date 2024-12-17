64632ad9531739efbec64c1b1e9356ae830ba210
Camilo Botero <cbotero2709@gmail.com>
2024-12-17 16:55:20 -0500

    Add basic action

--- app/controllers/api/v1/tin_validations_controller.rb
+++ app/controllers/api/v1/tin_validations_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::TinValidationsController < ApplicationController
-  def basic    
-    render json: {validation: true}, status: :ok
+  def basic
+    if params[:tin_validation][:input].present?
+      render json: { validation: true }, status: :ok
+    else
+      render json: { validation: false, status: 'failed' }, status: :not_acceptable
+    end
   end
 end
