9e8ef7443f8960db8a8f9b469d8e0f0c6c2ff6e6
Camilo Botero <cbotero2709@gmail.com>
2024-12-17 15:39:28 -0500

    Add first route for validation

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :api do
+    namespace :v1 do
+      namespace :tin_validation do
+        post :basic
+      end
+    end
+  end
 end
